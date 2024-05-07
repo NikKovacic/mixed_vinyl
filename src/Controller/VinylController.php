c2ef5f121523b48cbf71309d1c393d126277e20d
NikKovacic <nik.kovacic10@gmail.com>
2024-05-07 20:10:17 +0200

    Wildcard Routes

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -6,11 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use function Symfony\Component\String\u;
+
 class VinylController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route('/', name: 'app_vinyl_homepage')]
     public function homepage(): Response
     {
         return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}', name: 'app_vinyl_browse')]
+    public function browse(?string $slug = null): Response
+    {
+        $title = 'All Genres';
+        if ($slug) {
+            $title = u(str_replace('-', ' ', $slug))->title();
+        }
+
+        return new Response($title);
     }
 }
