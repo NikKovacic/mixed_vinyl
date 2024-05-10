550560201cc204c3b52601bd14f65c24c7445081
NikKovacic <nik.kovacic10@gmail.com>
2024-05-10 08:04:23 +0200

    Twig & inheritance

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -13,7 +13,19 @@ class VinylController extends AbstractController
     #[Route('/', name: 'app_vinyl_homepage')]
     public function homepage(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Gansta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
+        ]);
     }
 
     #[Route('/browse/{slug}', name: 'app_vinyl_browse')]
