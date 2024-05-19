33c191fc17a868d120be7aba20ff4b44dc883175
NikKovacic <nik.kovacic10@gmail.com>
2024-05-19 15:33:21 +0200

    Manual Service Config in services.yaml

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -10,17 +10,19 @@ class MixRepository
 {
     private HttpClientInterface $httpClient;
     private CacheInterface $cache;
+    private bool $isDebug;
 
-    public function __construct(HttpClientInterface $httpClient, CacheInterface $cache)
+    public function __construct(HttpClientInterface $httpClient, CacheInterface $cache, bool $isDebug)
     {
         $this->httpClient = $httpClient;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
     }
 
     public function findAll()
     {
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
-            $cacheItem->expiresAfter(5);
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
 
             return $response->toArray();
