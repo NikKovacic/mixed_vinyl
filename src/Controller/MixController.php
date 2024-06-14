df8990273696b257666ed7e8e98343c551f4a3da
NikKovacic <nik.kovacic10@gmail.com>
2024-06-14 11:40:34 +0200

    The Request Object & Updating an Entity

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\VinylMix;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -36,6 +37,25 @@ class MixController extends AbstractController
     {
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
+        ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $direction = $request->get('direction', 'up');
+        if ('up' === $direction) {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+
+        // Persist is redundant because mix object already exist!
+        // $entityManager->persist($mix);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_mix_show', [
+            'id' => $mix->getId(),
         ]);
     }
 }
